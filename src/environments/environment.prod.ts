628e0c6c3a61cd9cd1a26036462e54759804c06c
angularamadeus159 <angularamadeus159@gmail.com>
2026-02-03 00:23:38 -0500

    edicion en url de backend desplegado en render.com

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://tu-backend-url.com/api', // Cambia por tu URL real de backend
+  apiUrl: 'https://travel-agency-back-gu7f.onrender.com/api',
   supabaseUrl: 'https://nfyufytfpqhivuievnqc.supabase.co',
   supabaseKey: 'sb_publishable_RMKatkEOo5iPSiGOZlJYyQ_tIpfVtp5'
 };
