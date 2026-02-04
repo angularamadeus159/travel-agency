687082455c7b887d1dfcbc044b368385eeddd139
angularamadeus159 <angularamadeus159@gmail.com>
2026-02-03 21:06:35 -0500

    cambio de apiUrl por railway

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://travel-agency-back-gu7f.onrender.com/api',
+  apiUrl: 'https://travel-agency-back-production.up.railway.app/api',
   supabaseUrl: 'https://nfyufytfpqhivuievnqc.supabase.co',
   supabaseKey: 'sb_publishable_RMKatkEOo5iPSiGOZlJYyQ_tIpfVtp5'
 };
